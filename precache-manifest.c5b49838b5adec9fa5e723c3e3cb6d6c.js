91fb7e40b4b8af9b9de5af4d0db3739fbdcbe741
Favian <favian@Favians-MacBook-Pro.local>
2019-01-19 23:36:57 +0700

    tes

---
+++ precache-manifest.c5b49838b5adec9fa5e723c3e3cb6d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33ffa4e02e96e4767d4e",
+    "url": "/static/js/main.33ffa4e0.chunk.js"
+  },
+  {
+    "revision": "70ff7dbed7028b107527",
+    "url": "/static/js/1.70ff7dbe.chunk.js"
+  },
+  {
+    "revision": "33ffa4e02e96e4767d4e",
+    "url": "/static/css/main.75f1e086.chunk.css"
+  },
+  {
+    "revision": "1a1f147a3b758f9e9827b5211c0353a4",
+    "url": "/index.html"
+  }
+];
